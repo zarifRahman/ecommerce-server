cbf865ccc7caea34c8f912f50ac74010790fb6eb
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-01-25 02:04:24 +0600

    login and register response not working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
   // we can pass json file
 app.use(express.json());
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const User = require("../modals/User");
+const CryptoJS = require("crypto-js");
+
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    // use crypto js for password
+    password: req.body.password,
+  });
+  try {
+    // send it to DB
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+    
+  }catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    // find user
+    const user = await User.findOne({username: req.body.username});
+
+    !user && res.status(401).json("Wrong Credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf16);
+    password !== req.body.password && res.status(401).json("Wrong User Name");
+
+    res.status(200).json(user);
+
+  }catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+module.exports = router;
